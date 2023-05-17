0280bb5c1c55c0ccbb4f3267cd816e21c58932c6
vikrant <vikrant1k978@gmail.com>
2023-05-17 20:58:08 +0530

    updated folder structure

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser'
 import http from 'http'
 import mongoose from 'mongoose'
 import 'dotenv/config'
-import routes from "./src/routes/index.js"
+import routes from "../src/routes/index.js"
 
 const app = express()
 app.use(cors())
